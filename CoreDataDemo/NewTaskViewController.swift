416ed500056298af2db753d1a8c079fe3367fcf3
ilabrosimov <ilabrosimov@gmail.com>
2021-07-06 11:07:32 +0300

    Добавил NewTaskViewController

--- CoreDataDemo/NewTaskViewController.swift
+++ CoreDataDemo/NewTaskViewController.swift
@@ -1,19 +1,15 @@
 //
-//  ViewController.swift
+//  File.swift
 //  CoreDataDemo
 //
 //  Created by ilabrosimov on 06.07.2021.
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+final class NewTaskViewController:  UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
     }
-
-
 }
-
